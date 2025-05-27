9e096afe3cf43485b88bd232447dbdff39c77545
shelken <shelken.pxk@gmail.com>
2025-05-28 05:13:17 +0800

    更新entrypoint.sh和config.yaml以使用环境变量ECS_LOCAL_IPV4和ECS_LOCAL_IPV6，确保默认值的设置和替换逻辑正确

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -3,13 +3,30 @@
 # crond
 /usr/sbin/crond -b -l 8
 
+ecs_local_v4=$ECS_LOCAL_IPV4
+ecs_local_v6=$ECS_LOCAL_IPV6
+private_dns_server=$PRIVATE_DNS_SERVER
+
 # 检查环境变量$PRIVATE_DNS_SERVER是否设置
-if [ -z "$PRIVATE_DNS_SERVER" ]; then
-  echo "Error: Environment variable PRIVATE_DNS_SERVER is not set."
-  exit 1
+if [ -z "$private_dns_server" ]; then
+    # 如果没有设置PRIVATE_DNS_SERVER环境变量，则使用 easymosdns 的默认值
+    private_dns_server="https://mosdns.apad.pro/api-query"
+    ecs_local_v4="101.6.6.0"
+    ecs_local_v6="2001:da8::"
 fi
 
-# 替换/etc/mosdns/config.yaml中的变量$PRIVATE_DNS_SERVER
-sed -i "s#\$PRIVATE_DNS_SERVER#$PRIVATE_DNS_SERVER#g" /etc/mosdns/config.yaml
+# 检查环境变量$ecs_local是否设置
+if [ -z "$ecs_local_v4" ]; then
+    # 如果没有设置ECS_LOCAL_CIDR环境变量，则使用默认值
+    ecs_local_v4="120.80.1.0" # 广东联通
+fi
+if [ -z "$ecs_local_v6" ]; then
+    ecs_local_v6="2408:8459::" # 广东联通
+fi
+
+# 替换/etc/mosdns/config.yaml中的变量$private_dns_server
+sed -i "s#\$PRIVATE_DNS_SERVER#$private_dns_server#g" /etc/mosdns/config.yaml
+sed -i "s#\$ECS_LOCAL_IPV4#$ecs_local_v4#g" /etc/mosdns/config.yaml
+sed -i "s#\$ECS_LOCAL_IPV6#$ecs_local_v6#g" /etc/mosdns/config.yaml
 
 /usr/bin/mosdns start --dir /etc/mosdns
